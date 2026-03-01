d80e4530f4a443e9719d1a7294bf430c1a0e3678
gopikakusuma <gopikakusuma@gmail.com>
2026-03-01 21:11:20 +0530

    Create delete.java

---
+++ delete.java
@@ -0,0 +1,33 @@
+package fullstack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class delete{
+
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+          try {
+      DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+      Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/me","root","1234");
+
+      Statement statement = connection.createStatement();
+      int rs = statement.executeUpdate("delete from emp where empname='ram' ");
+
+    
+          System.out.println(rs);
+      
+      connection.close();
+          }
+
+          catch(SQLException e) {
+                  e.printStackTrace();
+          }
+
+
+        }
+
+}
